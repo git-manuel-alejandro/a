306fc537472500138f436831b744e5dfbadccdc2
Manuel Roa <manuel.roaojeda23@gmail.com>
2022-02-02 17:03:00 -0300

    department model

--- applications/departamento/models.py
+++ applications/departamento/models.py
@@ -1,3 +1,12 @@
+from statistics import mode
 from django.db import models
 
 # Create your models here.
+
+class Departamento(models.Model):
+    name = models.CharField('Nombre', max_length=50)
+    short_name = models.CharField('Nombre corto' , max_length=50)
+    active = models.BooleanField('Anulado', default=False)
+
+    def __str__(self):
+        return str(self.id) + ' - ' + self.name + ' - ' +  self.short_name
