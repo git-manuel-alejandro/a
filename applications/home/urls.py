92a9362b809d9e6e24e26f05fb61672ef9954cb4
Manuel Roa <manuel.roaojeda23@gmail.com>
2022-02-02 16:39:24 -0300

    class list view

--- applications/home/urls.py
+++ applications/home/urls.py
@@ -7,4 +7,6 @@ from .import views
 urlpatterns = [
     path('prueba/', views.Prueba.as_view()),
     path('lista/', views.PruebaListView.as_view()),
+    path('lista-prueba/', views.ListarPruebaListView.as_view()),
+    path('add/', views.PruebaCreateView.as_view()),
 ]
--- applications/home/views.py
+++ applications/home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView , CreateView
+from .import models
 
 # Create your views here.
 
@@ -12,3 +13,16 @@ class PruebaListView(ListView):
     template_name = "home/lista.html"
     context_object_name = 'lista'
     queryset = ['0', '10', '20','30']
+
+
+
+class ListarPruebaListView(ListView):
+    model = models.Prueba
+    template_name = "home/lista_prueba.html"
+    context_object_name = 'lista'
+
+
+class PruebaCreateView(CreateView):
+    model = models.Prueba
+    template_name = "home/add.html"
+    fields = '__all__'
