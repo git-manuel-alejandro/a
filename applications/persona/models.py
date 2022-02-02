29cca7eab8b34c8349ef092bd785701eb1002888
Manuel Roa <manuel.roaojeda23@gmail.com>
2022-02-02 17:43:52 -0300

    conecting to postgresql

--- applications/persona/models.py
+++ applications/persona/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from applications.departamento.models import Departamento
 
 # Create your models here.
+
+JOB_CHOICES = [
+    ('0', 'CONTADOR'),
+    ('1', 'ADMINISTRADOR'),
+    ('2', 'ECONOMISTA'),
+    ('3', 'Django Dev'),
+]
+
+class Empleado(models.Model):
+    first_name = models.CharField('Nombres', max_length=60)
+    last_name = models.CharField('Apellidos', max_length=60)
+    job = models.CharField('Trabajo' ,max_length=51, choices=JOB_CHOICES)
+    departamento = models.ForeignKey(Departamento , on_delete=models.CASCADE)
+    # image = models.ImageField()
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
